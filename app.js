cc3c03d1aea552000d3a10df3a9436eeaa9fb027
sameerkindia <sameerkhanaa5@gmail.com>
2024-08-20 16:19:15 +0530

    Arrays in ts

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var App = /** @class */ (function () {
-    function App() {
-    }
-    App.prototype.test = function () {
-        console.log("class working");
-    };
-    return App;
-}());
-var a = new App();
-a.test();
+// class App {
+//   test() {
+//     console.log("class working");
+//   }
+// }
+// let a = new App();
+// a.test();
+////////////////////////////////
+////////////////////////////////
+// Arrays
+////////////////////////////////
+////////////////////////////////
+var array = ["sameer", "khan", "pali", 1, false, [""]];
+array.push("ts");
+array.push(100);
+array.push(true);
+array.push(["hello"]);
+// array.push({});
+console.log(array);
--- app.ts
+++ app.ts
@@ -1,10 +1,30 @@
 export {};
-class App {
-  test() {
-    console.log("class working");
-  }
-}
+// class App {
+//   test() {
+//     console.log("class working");
+//   }
+// }
 
-let a = new App();
+// let a = new App();
 
-a.test();
+// a.test();
+
+////////////////////////////////
+////////////////////////////////
+// Arrays
+////////////////////////////////
+////////////////////////////////
+
+let array = ["sameer", "khan", "pali", 1, false, [""]];
+
+array.push("ts");
+
+array.push(100);
+
+array.push(true);
+
+array.push(["hello"]);
+
+// array.push({});
+
+console.log(array);
