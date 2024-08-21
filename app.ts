73b7c844ae69567cdbc15c3ac1b6ded8d4a0bcaa
sameerkindia <sameerkhanaa5@gmail.com>
2024-08-21 12:47:44 +0530

    union type

--- app.ts
+++ app.ts
@@ -35,25 +35,30 @@ export {};
 ////////////////////////////////
 ////////////////////////////////
 
-interface usersTyped {
-  name: string;
-  // age: number;
-  age: any;
-  city: string;
-}
+// interface usersTyped {
+//   name: string;
+//   // age: number;
+//   age: any;
+//   city: string;
+// }
 
-let users1: usersTyped = {
-  name: "sameer khan",
-  age: 24,
-  city: "pali",
-};
+// let users1: usersTyped = {
+//   name: "sameer khan",
+//   age: 24,
+//   city: "pali",
+// };
 
-let users2: any = {
-  name: "sameer khan",
-  age: 24,
-  city: "pali",
-};
+// let users2: any = {
+//   name: "sameer khan",
+//   age: 24,
+//   city: "pali",
+// };
 
-users2.job = "web developer";
+// users2.job = "web developer";
 
-console.log(users2);
+// console.log(users2);
+
+/////////////// Union Type
+let id: number | string;
+id = 101;
+id = "E101";
